4d2d205fcc39c92655827b2e468a278e9f99d01d
Dev_Rokhaya <rokhaya.codes@gmail.com>
2021-08-12 14:49:11 -0500

    created funtions + logic in script.js

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget(){
+let rndNum = Math.floor(Math.random() * 10);
+console.log(rndNum)
+};
+
+function compareGuesses(humanGuess, computerGuess, secretTargetGuess ){
+
+    let humanGuess = Math.floor(Math.random() * 10);
+    let computerGuess = Math.floor(Math.random() * 10);
+    let secretTargetGuess = generateTarget();
+
+    console.log(humanGuess);
+    console.log(computerGuess);
+    console.log(secretTargetGuess);
+
+    let humanVal = Math.abs(secretTargetGuess - humanGuess);
+    let computerVal = Math.abs(secretTargetGuess - computerGuess);
+
+if(humanVal === computerVal){
+    return true ;
+}else if (humanVal > computerVal){
+    return true ;
+}else if(computerValn < humanVal){
+    return false;
+}
+
+};
